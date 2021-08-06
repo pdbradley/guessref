7d0180972ecd29d8783792b40feace19d6f17891
dinushkaherath <dinushkaherath@gmail.com>
2021-08-05 22:58:31 -0400

    2x2 grid w/ color & center as Notifications

--- src/VerseAnswers.js
+++ src/VerseAnswers.js
@@ -1,33 +1,32 @@
 import React from 'react';
 import 'bulma/css/bulma.min.css';
-import {Tile, Section, Box} from 'react-bulma-components';
+import { Heading, Tile, Section, Box, Notification, Columns } from 'react-bulma-components';
 
-const VerseAnswers = ({verse_answers}) => {
-return (
-    <Section>
-        <Box>
-            <Tile vertical>
-                <Tile horizontal>
-                    <Tile >
-                        {verse_answers[0].label}
+const VerseAnswers = ({ verse_answers }) => {
+    return (
+        <Section>
+            <Box>
+                <Tile>
+                    <Tile kind="parent" vertical>
+                        <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
+                            {verse_answers[0].label}
+                        </Tile>
+                        <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
+                            {verse_answers[1].label}
+                        </Tile>
                     </Tile>
-                    <Tile>
-                        {verse_answers[1].label}
+                    <Tile kind="parent" vertical>
+                        <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
+                            {verse_answers[2].label}
+                        </Tile>
+                        <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
+                            {verse_answers[3].label}
+                        </Tile>
                     </Tile>
                 </Tile>
-                <Tile horizontal>
-                    <Tile>
-                        {verse_answers[2].label}
-                    </Tile>
-                    <Tile>
-                        {verse_answers[3].label}
-                    </Tile>
-                </Tile>
-            </Tile> 
-        </Box>
-    </Section>   
-    // bulma 2x2 grid for the four verse answers (db-unaware)
-);
+            </Box>
+        </Section>
+    );
 };
 
 export default VerseAnswers;
