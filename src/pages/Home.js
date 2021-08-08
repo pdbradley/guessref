851f693e0be0be4c841234d2afdc42022c0e8d08
dinushkaherath <dinushkaherath@gmail.com>
2021-08-08 08:56:57 -0400

    Login component (add username to cookie) and universal-cookie install

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import GamesQuery from '../GamesQuery';
 import Header from '../layout/Header';
+import Login from '../Login';
+import Cookies from 'universal-cookie';
 import 'bulma/css/bulma.min.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import {
   Navbar,
   Hero,
@@ -12,10 +14,13 @@ import {
 } from 'react-bulma-components';
 
 const Home = () => {
+  const cookies = new Cookies();
+
   return (
     <>
       <Header />
       <GamesQuery />
+      <Login usernameExists={cookies.get('username') != null} />
     </>
   );
 };
