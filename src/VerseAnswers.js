58acc393fa0450128bbdc94b2b204a314385f9c0
dinushkaherath <dinushkaherath@gmail.com>
2021-08-08 09:56:29 -0400

    change VerseAnswers to use Heading component

--- src/VerseAnswers.js
+++ src/VerseAnswers.js
@@ -1,32 +1,32 @@
 import React from 'react';
 import 'bulma/css/bulma.min.css';
-import { Heading, Tile, Section, Box, Notification, Columns } from 'react-bulma-components';
+import { Heading, Tile, Section, Box, Notification } from 'react-bulma-components';
 
 const VerseAnswers = ({ verse_answers }) => {
-    return (
-        <Section>
-            <Box>
-                <Tile>
-                    <Tile kind="parent" vertical>
-                        <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
-                            {verse_answers[0].label}
-                        </Tile>
-                        <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
-                            {verse_answers[1].label}
-                        </Tile>
-                    </Tile>
-                    <Tile kind="parent" vertical>
-                        <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
-                            {verse_answers[2].label}
-                        </Tile>
-                        <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
-                            {verse_answers[3].label}
-                        </Tile>
-                    </Tile>
-                </Tile>
-            </Box>
-        </Section>
-    );
+  return (
+    <Section>
+      <Box>
+        <Tile>
+          <Tile kind="parent" vertical>
+            <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
+              <Heading>{verse_answers[0].label}</Heading>
+            </Tile>
+            <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
+              <Heading>{verse_answers[1].label}</Heading>
+            </Tile>
+          </Tile>
+          <Tile kind="parent" vertical>
+            <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
+              <Heading>{verse_answers[2].label}</Heading>
+            </Tile>
+            <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
+              <Heading>{verse_answers[3].label}</Heading>
+            </Tile>
+          </Tile>
+        </Tile>
+      </Box>
+    </Section>
+  );
 };
 
 export default VerseAnswers;
