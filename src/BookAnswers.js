912bf18f14f60482ab159048d8949d602abdefc6
dinushkaherath <dinushkaherath@gmail.com>
2021-08-08 18:03:52 -0400

    use onClick Handler from parent

--- src/BookAnswers.js
+++ src/BookAnswers.js
@@ -3,24 +3,28 @@ import 'bulma/css/bulma.min.css';
 import { Tile, Section, Box, Notification } from 'react-bulma-components';
 import BookLabelFromNumber from './BookLabelFromNumber';
 
-const BookAnswers = ({ book_answers }) => {
+const BookAnswers = ({ onAnswerClicked, book_answers }) => {
+  function handleClick(answer) {
+    onAnswerClicked(book_answers[answer])
+  }
+
   return (
     <Section>
       <Box>
         <Tile>
           <Tile kind="parent" vertical>
-            <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
+            <Tile kind="child" onClick={() => handleClick(0)} renderAs={Notification} color="success" textAlign="center">
               <BookLabelFromNumber book_number={book_answers[0].book_number} />
             </Tile>
-            <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
+            <Tile kind="child" onClick={() => handleClick(1)} renderAs={Notification} color="success" textAlign="center">
               <BookLabelFromNumber book_number={book_answers[1].book_number} />
             </Tile>
           </Tile>
           <Tile kind="parent" vertical>
-            <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
+            <Tile kind="child" onClick={() => handleClick(2)} renderAs={Notification} color="success" textAlign="center">
               <BookLabelFromNumber book_number={book_answers[2].book_number} />
             </Tile>
-            <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
+            <Tile kind="child" onClick={() => handleClick(3)} renderAs={Notification} color="success" textAlign="center">
               <BookLabelFromNumber book_number={book_answers[3].book_number} />
             </Tile>
           </Tile>
--- src/VerseAnswers.js
+++ src/VerseAnswers.js
@@ -2,24 +2,28 @@ import React from 'react';
 import 'bulma/css/bulma.min.css';
 import { Heading, Tile, Section, Box, Notification } from 'react-bulma-components';
 
-const VerseAnswers = ({ verse_answers }) => {
+const VerseAnswers = ({ onAnswerClicked, verse_answers }) => {
+  function handleClick(answer) {
+    onAnswerClicked(verse_answers[answer])
+  }
+
   return (
     <Section>
       <Box>
         <Tile>
           <Tile kind="parent" vertical>
-            <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
+            <Tile kind="child" onClick={() => handleClick(0)} renderAs={Notification} color="success" textAlign="center">
               <Heading>{verse_answers[0].label}</Heading>
             </Tile>
-            <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
+            <Tile kind="child" onClick={() => handleClick(1)} renderAs={Notification} color="success" textAlign="center">
               <Heading>{verse_answers[1].label}</Heading>
             </Tile>
           </Tile>
           <Tile kind="parent" vertical>
-            <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
+            <Tile kind="child" onClick={() => handleClick(2)} renderAs={Notification} color="success" textAlign="center">
               <Heading>{verse_answers[2].label}</Heading>
             </Tile>
-            <Tile kind="child" renderAs={Notification} color="success" textAlign="center">
+            <Tile kind="child" onClick={() => handleClick(3)} renderAs={Notification} color="success" textAlign="center">
               <Heading>{verse_answers[3].label}</Heading>
             </Tile>
           </Tile>
