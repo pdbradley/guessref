5bdc8b4d592f6bd5b5e1f7a4b6a8eb9e7834db42
dinushkaherath <dinushka.herath@gmail.com>
2021-08-13 14:38:20 -0400

    order verse_words by created_at

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,10 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import 'bulma/css/bulma.min.css';
-import {useSubscription, gql} from '@apollo/client';
+import { useSubscription, gql } from '@apollo/client';
 import GameSession from './GameSession';
 import CustomNavbar from './layout/CustomNavbar';
 import CustomHero from './layout/CustomHero';
-import {useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 const GET_GAME = gql`
   subscription getGame($uuid: uuid) {
@@ -38,7 +38,7 @@ const GET_GAME = gql`
             correct
             label
           }
-          verse_words {
+          verse_words(order_by: {created_at: asc}) {
             id
             word_text
             visible
@@ -50,9 +50,9 @@ const GET_GAME = gql`
 `;
 
 const Game = () => {
-  const {uuid} = useParams();
-  const {loading, error, data} = useSubscription(GET_GAME, {
-    variables: {uuid: uuid},
+  const { uuid } = useParams();
+  const { loading, error, data } = useSubscription(GET_GAME, {
+    variables: { uuid: uuid },
   });
 
   if (loading) {
