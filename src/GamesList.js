517ca6411a7e721476532dc264b90f67e85891f8
dinushkaherath <dinushkaherath@gmail.com>
2021-08-13 06:50:10 -0400

    display game info

--- src/GamesList.js
+++ src/GamesList.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import 'bulma/css/bulma.min.css';
-import {Section, Box, Tile} from 'react-bulma-components';
+import { Section, Box, Tile, Heading } from 'react-bulma-components';
 import JoinGameButton from './JoinGameButton';
 import NewGame from './NewGame';
 
-const GamesList = ({game_sessions}) => {
+const GamesList = ({ game_sessions }) => {
   return (
     <Section>
-      <Tile kind="parent" vertical>
-        <Tile kind="child" textAlign="center">
-          <Box>
-            <NewGame />
-          </Box>
-        </Tile>
+      <Tile kind="ancestor" textAlign="center" vertical>
+        <Box>
+          <NewGame />
+        </Box>
         {game_sessions.map(gs => (
           <Box>
-            <Tile kind="parent">
-              <Tile kind="child" textAlign="center">
-                Game {gs.id} {gs.name}
-              </Tile>
-              <Tile kind="child" textAlign="center">
-                <JoinGameButton game_session_uuid={gs.uuid} />
+            <Tile vertical>
+              <Tile>
+                <Tile kind="parent">
+                  <Tile kind="child">
+                    <Heading>{gs.name}, Game {gs.id}, Status {gs.status}</Heading>
+                    <Heading subtitle>{gs.participants_aggregate.aggregate.count} Participants, {gs.game_rounds_aggregate.aggregate.count} Rounds, {gs.game_rounds.reduce((accumulator, item) =>
+                      accumulator + item.verses_aggregate.aggregate.count, 0)} Total Verses</Heading>
+                  </Tile>
+                </Tile>
+                <Tile kind="parent">
+                  <Tile kind="child">
+                    <JoinGameButton game_session_uuid={gs.uuid} />
+                  </Tile>
+                </Tile>
               </Tile>
             </Tile>
           </Box>
