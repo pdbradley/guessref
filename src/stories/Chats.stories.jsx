f8318847c117fad632665d12c2ff84b0869d73ed
dinushkaherath <dinushka.herath@gmail.com>
2021-08-13 12:20:32 -0400

    update chat story to work

--- src/stories/Chats.stories.jsx
+++ src/stories/Chats.stories.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Chats from '../Chats';
 
 export default {
@@ -12,9 +11,9 @@ const Template = args => <Chats {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   chats: [
-    {id: 1, author: 'Robert', content: 'Howdy howdy!'},
-    {id: 2, author: 'Bill', content: 'Hello from Phil'},
-    {id: 3, author: 'Oleg', content: 'Hello from Oleg'},
-    {id: 4, author: 'Andreas', content: 'Hello Andreas'},
+    { id: 1, author: 'Robert', message: 'Howdy howdy!' },
+    { id: 2, author: 'Bill', message: 'Hello from Phil' },
+    { id: 3, author: 'Oleg', message: 'Hello from Oleg' },
+    { id: 4, author: 'Andreas', message: 'Hello Andreas' },
   ],
 };
