faffd2c6fb6e5aaa4f9e8f11b7c7899763ae585c
JCTalbott <jctalbott2@gmail.com>
2021-08-14 14:59:47 -0400

    Log out button (clears cookies, too) and redirects to log in page

--- src/layout/CustomNavbar.js
+++ src/layout/CustomNavbar.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import 'bulma/css/bulma.min.css';
 import {Navbar} from 'react-bulma-components';
+import Cookies from 'universal-cookie';
 
 const CustomNavbar = () => {
+  const cookies = new Cookies();
+
+  const usernameExists = cookies.get('username') != null;
+
+  function handleOnClick(e) {
+    cookies.remove('username', { path: '/' });
+    cookies.remove('user_uuid', { path: '/' });
+    cookies.remove('game_session_uuid', { path: '/' });
+    window.location = '/login';
+  }
+
   return (
     <Navbar>
-      <Navbar.Item href="/home">Games </Navbar.Item>
-      <Navbar.Item href="/new_game">New Game </Navbar.Item>
+      <Navbar.Container align="left">
+        <Navbar.Item href="/home">Games </Navbar.Item>
+        <Navbar.Item href="/new_game">New Game </Navbar.Item>
+      </Navbar.Container>
+      
+      {usernameExists ? <Navbar.Container align="right">
+        <Navbar.Item href="#" onClick={handleOnClick}>
+          Log Out
+        </Navbar.Item>
+      </Navbar.Container> : <></>}
     </Navbar>
   );
 };
