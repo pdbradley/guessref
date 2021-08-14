de19ce01853993190f8eebf5e8dbe43a5fc61af0
JCTalbott <jctalbott2@gmail.com>
2021-08-14 17:24:55 -0400

    initializeScore of 0 when joining game

--- src/JoinGameButton.js
+++ src/JoinGameButton.js
@@ -20,6 +20,7 @@ const JoinGameButton = ({game_session_uuid}) => {
         console.log(data);
         window.location = `/game/${game_session_uuid}`;
         cookies.set('game_session_uuid', game_session_uuid, {path: '/'});
+        initializeScore();
       })
       .catch(error => {
         console.log(error);
@@ -27,6 +28,23 @@ const JoinGameButton = ({game_session_uuid}) => {
           window.location = `/game/${game_session_uuid}`;
         }
       });
+  }
+
+  function initializeScore() {
+    fetch(`${process.env.REACT_APP_HASURA_REST_API}/insert_game_session_score`, {
+      method: 'POST',
+      body: JSON.stringify({
+        game_session_uuid: game_session_uuid,
+        user_uuid: user_uuid,
+        score: 0
+      })
+    }).then(response =>
+      response.json()
+    ).then(data => {
+      console.log(data);
+    }).catch(error => {
+      console.log(error);
+    })
   }
 
   return (
