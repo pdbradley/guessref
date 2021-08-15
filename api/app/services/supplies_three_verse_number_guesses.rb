90b5f9acb63f34ffb6e214ebf85b728aa851f392
JCTalbott <jctalbott2@gmail.com>
2021-08-15 03:26:52 -0400

    implement lookup table to cap verses/chapters

--- api/app/services/supplies_three_verse_number_guesses.rb
+++ api/app/services/supplies_three_verse_number_guesses.rb
@@ -1,11 +1,14 @@
 class SuppliesThreeVerseNumberGuesses
-    def initialize(verse_number)
+    def initialize(book_number, chapter_number, verse_number)
+        @book_number = book_number
+        @chapter_number = chapter_number
         @verse_number = verse_number
     end
 
     def guesses
+        max_verses = MaxNumVersesChaptersLookup.get_max_verses(@book_number, @chapter_number)
         sample_from_array = ([*(@verse_number-5)..(@verse_number+5)] - [@verse_number])
-                            .select{ |number| number > 0 }
+                            .select{ |n| n > 0 && n < max_verses }
         sample_from_array.sample(3)
     end
 end
