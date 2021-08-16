ebe1f54a2a4f4952706b6775cd501addf332e97d
Philip Bradley <pdbradley@gmail.com>
2021-08-16 08:08:33 -0400

    small timing adjustments

--- api/app/models/verse.rb
+++ api/app/models/verse.rb
@@ -56,16 +56,16 @@ class Verse < ApplicationRecord
 
   def queued_tick!
     set_active!
-    VerseTickJob.set(wait: (1).seconds).perform_later(self.uuid)
+    VerseTickJob.set(wait: (0.7).seconds).perform_later(self.uuid)
   end
 
   def active_tick!
     if next_hidden_word
       next_hidden_word.visible! 
-      VerseTickJob.set(wait: (1).seconds).perform_later(self.uuid)
+      VerseTickJob.set(wait: (0.7).seconds).perform_later(self.uuid)
     else
       set_revealed!
-      VerseTickJob.set(wait: (8).seconds).perform_later(self.uuid)
+      VerseTickJob.set(wait: (5).seconds).perform_later(self.uuid)
     end
   end
 
