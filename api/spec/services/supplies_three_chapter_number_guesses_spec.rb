50371013ae5f415671abcff1d98050ca6bc57309
dinushkaherath <dinushka.herath@gmail.com>
2021-08-15 23:35:14 -0400

    default 1-4 for vvs/ch with less than 4 options

--- api/spec/services/supplies_three_chapter_number_guesses_spec.rb
+++ api/spec/services/supplies_three_chapter_number_guesses_spec.rb
@@ -2,12 +2,17 @@ require 'rails_helper'
 
 RSpec.describe SuppliesThreeChapterNumberGuesses do
     describe ".guesses" do
-        it "returns an array of three unique numbers" do
-            result = SuppliesThreeChapterNumberGuesses.new(40, 1).guesses
-
-            expect(result.length).to eq 3
-            expect(result.uniq.length).to eq 3
-            expect(result.all? { |i| i.is_a?(Integer) }).to eq true
+        it "returns an array of three guesses for each chapter of the new testament" do
+            (40..66).each do |book_number|
+                max_chapters = MaxNumVersesChaptersLookup.get_max_chapters(book_number)
+                (1..max_chapters).each do |chapter_number|
+                    guesses = SuppliesThreeChapterNumberGuesses.new(book_number, chapter_number).guesses
+                    expect(guesses.length).to eq 3
+                    expect(guesses.uniq.length).to eq 3
+                    expect(guesses).not_to include chapter_number
+                    expect(guesses.all? { |i| i.is_a?(Integer) }).to eq true
+                end
+            end
         end
         it "returns an array of numbers close to inputted chapter number" do
             result = SuppliesThreeChapterNumberGuesses.new(40, 20).guesses
