abf7dd5ef2bf346f6b25e3fd928c939f977a8707
Philip Bradley <pdbradley@gmail.com>
2021-08-16 08:14:13 -0400

    remove hasura metadata apply from release tasks

--- api/release-tasks.sh
+++ api/release-tasks.sh
@@ -1,5 +1,2 @@
 export NODE_OPTIONS=
 bundle exec rake db:migrate &&
-npm install --global hasura-cli@2.0.0-alpha.10 &&
-cd hasura &&
-hasura metadata apply --admin-secret $HASURA_ADMIN_SECRET --endpoint $HASURA_ENDPOINT
