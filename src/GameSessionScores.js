f4303096e034d387eb695edc6c07e4b0ce574542
dinushkaherath <dinushka.herath@gmail.com>
2021-08-22 14:01:10 -0400

    sort by user_name

--- src/GameSessionScores.js
+++ src/GameSessionScores.js
@@ -4,17 +4,19 @@ import { Panel } from 'react-bulma-components';
 
 const GameSessionScores = ({ game_session_scores }) => {
   let combinedScores = game_session_scores.reduce(function (a, o) {
-    a[o.user_uuid] = a[o.user_uuid] || { score: 0 };
-    a[o.user_uuid].user_uuid = o.user_uuid;
-    a[o.user_uuid].user_name = o.user.name;
-    a[o.user_uuid].score += o.score;
+    a[o.user.name] = a[o.user.name] || { score: 0 };
+    a[o.user.name].user_uuid = o.user_uuid;
+    a[o.user.name].user_name = o.user.name;
+    a[o.user.name].score += o.score;
     return a;
   }, Object.create(null));
+
+  let sortedScores = Object.keys(combinedScores).sort().reduce((r, k) => (r[k] = combinedScores[k], r), {});
 
   return (
     <Panel>
       <Panel.Header>Scores</Panel.Header>
-      {Object.values(combinedScores).map(({ user_uuid, user_name, score }) => (
+      {Object.values(sortedScores).map(({ user_uuid, user_name, score }) => (
         <Panel.Block key={user_uuid}>
           {user_name}:{" "}{score}
         </Panel.Block>
