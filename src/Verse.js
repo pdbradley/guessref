37eec28cd50261f6b66c7473a2dc319f566c0bec
dinushkaherath <dinushka.herath@gmail.com>
2021-08-25 15:50:33 -0400

    set cookie and no refresh

--- src/Verse.js
+++ src/Verse.js
@@ -8,10 +8,7 @@ const Verse = ({ verse }) => {
     return <div>Waiting on Verse</div>;
   }
   if (verse.status === 'REVEALED') {
-    let book_answer = verse.book_answers[verse.book_answers.map(ba => ba.correct).indexOf(true)].label;
-    let chapter_answer = verse.chapter_answers[verse.chapter_answers.map(ba => ba.correct).indexOf(true)].label;
-    let verse_answer = verse.verse_answers[verse.verse_answers.map(ba => ba.correct).indexOf(true)].label;
-    return <VerseAnswer book={book_answer} chapter={chapter_answer} verse={verse_answer} />;
+    return <VerseAnswer verse={verse} />;
   }
   return (
     <div className="content">
