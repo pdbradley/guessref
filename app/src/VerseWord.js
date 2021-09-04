58712b6b354ed44b45ed414f34f542d969e9bedd
dinushkaherath <dinushka.herath@gmail.com>
2021-09-04 11:44:02 -0400

    have a fixed width visible/invisible

--- app/src/VerseWord.js
+++ app/src/VerseWord.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import 'bulma/css/bulma.min.css';
-import {Tag} from 'react-bulma-components';
+import { Tag } from 'react-bulma-components';
 
-const VerseWord = ({wordText, visible = true}) => {
-  const blankWord = new Array(wordText.length + 2).join('_');
-  //return <span class="tag">{visible ? wordText : blankWord}</span>;
+const VerseWord = ({ wordText, visible = true }) => {
+  const word_length_plus_two = wordText.length + 2;
+  const blankWord = new Array(word_length_plus_two).join('_');
   return (
-    <Tag size="large" color="text">
+    <Tag size="large" style={{ width: `${word_length_plus_two * 0.7}rem` }}>
       {visible ? wordText : blankWord}
     </Tag>
   );
