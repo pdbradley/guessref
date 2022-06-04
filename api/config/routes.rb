17fe72a3515b909a0097b3e5b09a094e2bf2bed4
Philip Bradley <pdbradley@gmail.com>
2022-06-04 16:33:43 -0400

    upgrade rails to 7

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  post '/games', to: 'games#create', as: 'new_game'
-  post '/start_game', to: 'games#start', as: 'start_game'
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
