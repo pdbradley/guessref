50969bd564edb9573197dd07a66ca4423e830df9
Philip Bradley <pdbradley@gmail.com>
2022-06-09 12:04:46 -0400

    set pages/home as root route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   post '/games', to: 'games#create', as: 'new_game'
   post '/start_game', to: 'games#start', as: 'start_game'
 
-  resource :session
+  root "pages#home"
 
+  resource :session
 end
