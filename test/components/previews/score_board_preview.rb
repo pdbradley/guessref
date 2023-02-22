554f756c70673b08fd1891f024f1c0d1919ff951
Philip Bradley <pdbradley@gmail.com>
2023-02-22 07:30:56 -0500

    primitive scoreboard component

--- test/components/previews/score_board_preview.rb
+++ test/components/previews/score_board_preview.rb
@@ -2,8 +2,14 @@ class ScoreBoardPreview < ViewComponent::Preview
 
   layout 'component_preview'
 
-  def basic
-    render(GameSessionScoreboardComponent.new({}))
+  def two_players
+    render(GameSessionScoreboardComponent.new(fake_scores(2)))
+  end
+
+  private
+
+  def fake_scores(num)
+    num.times.map { [Faker::Name.first_name, 12] }.to_h
   end
 
 end
