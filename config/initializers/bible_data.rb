d510c089b08467604ada516261fff6e5991650de
Philip Bradley <pdbradley@gmail.com>
2023-02-26 06:29:11 -0500

    move service classes into lib instead of models/concerns (#13)

--- config/initializers/bible_data.rb
+++ config/initializers/bible_data.rb
@@ -1,2 +1,2 @@
-require "#{Rails.root}/app/models/concerns/builds_book_number_keyed_hash.rb"
+require "#{Rails.root}/app/lib/builds_book_number_keyed_hash.rb"
 BIBLE_DATA = BuildsBookNumberKeyedHash.new.hash
