99dce60af93677ace669933aca6a452eefd46eeb
Philip Bradley <pdbradley@gmail.com>
2023-02-26 06:23:01 -0500

    fix bug when clicking on game when not logged in (#12)

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -7,15 +7,14 @@ RSpec.describe "Sessions", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+  describe "GET /any" do
+    it "forces a login if no session exists then redirects to original dest" do
+      game_session = create(:game_session)
 
-  # describe "logged out" do
-  #   it "redirects to new session path when joining a game but not logged in" do
-  #     game_session = create(:game_session)
-  #     get game_sessions_path
-  #
-  #     expect(response).to redirect_to sign_in_path
-  #   end
-  # end
-
-
+      get game_session_path(game_session)
+      expect(response).to redirect_to sign_in_path
+      post session_path, params: {name: 'Foo'}
+      expect(response).to redirect_to game_session_path(game_session)
+    end
+  end
 end
