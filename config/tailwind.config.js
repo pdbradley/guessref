b6f0e428911fd0354d19139ca6089efc85464062
Waseem Mehany <waseem@waseems-air.lan>
2023-07-05 20:18:00 -0500

    Initial design props

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,12 +10,23 @@ module.exports = {
   ],
   theme: {
     colors: {
+      white: "#fff",
+      black: "#000",
+      // primaryBlue: "#4d68ad",
+      primaryBlue: "#008cdd",
+      // primaryRed: "#f76b8a",
+      primaryRed: "rgb(154, 64, 93)",
+      secondaryRed: "rgb(249, 226, 221)",
+      textHeader: "#000000",
+      textSubheader: "#414552",
+      textParagraph: "#3c4257",
+      
       blue1: '#4a55a2',
       blue2: '#7895cb',
       blue3: '#a0bfe0',
       blue4: '#c5dff8',
       red: '#e63946',
-
+      "indigo-600": "#4a55a2",
 
     },
     extend: {
