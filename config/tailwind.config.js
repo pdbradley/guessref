da962da6ed5c20e94901b1a890c38026742faf58
Waseem Mehany <waseem@waseems-air.lan>
2023-07-06 12:51:34 -0500

    Updated tailwind custom colors and updated game-name screen

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,22 +10,28 @@ module.exports = {
   ],
   theme: {
     colors: {
+      // Basics
       white: "#fff",
       black: "#000",
-      primaryText: "rgb(50, 50, 90)",
-      primaryBlue: "#008cdd",
-      primaryRed: "rgb(204, 52, 70)",
-      secondaryRed: "rgb(249, 226, 221)",
+
+      // Default text
+      "text-100": "#000000",
+      "text-200": "#414552",
+      "text-300": "#3c4257",
+
+      // App colors
+      "blue-100": "#008cdd",
+      "green-100": "#008000",
+      "purple-100": "#673ab7",
+      "gray-100": "#e5e5e5",
+      "red-100": "#cc3446",
+      "red-300": "rgb(249, 226, 221)",
+      "indigo-100": "rgb(99, 91, 255)",
+      "slate-100": "#2c3e50",
       
+      // primaryText: "rgb(50, 50, 90)",
       // primaryBlue: "#4d68ad",
       // primaryRed: "#f76b8a",
-      textHeader: "#000000",
-      textSubheader: "#414552",
-      textParagraph: "#3c4257",
-
-
-      primaryIndigo: "rgb(99, 91, 255)",
-      
       blue1: '#4a55a2',
       blue2: '#7895cb',
       blue3: '#a0bfe0',
