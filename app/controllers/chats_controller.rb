dba0e8b17b598eddae7161a7171dc891cba699f9
Philip Bradley <pdbradley@gmail.com>
2023-07-07 13:24:01 -0700

    don't send blank line chats

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,7 +2,7 @@ class ChatsController < ApplicationController
 
   def create
     @game_session = GameSession.find params[:game_session_id]
-    @game_session.chat_messages.create(user: current_user, message: params[:message])
+    @game_session.chat_messages.create(user: current_user, message: params[:message]) unless params[:message].blank?
   end
 
 end
